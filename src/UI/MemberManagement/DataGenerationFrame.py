c4362330784413d3de0714aa5535d2ab1681c089
Ishaan Masil <darkknightishaan@gmail.com>
2023-09-10 18:40:11 -0500

    Added JSON clear ability

--- src/UI/MemberManagement/DataGenerationFrame.py
+++ src/UI/MemberManagement/DataGenerationFrame.py
@@ -9,42 +9,6 @@ from src.Constants import Constants
 from src.UI.Dashboard.MembersFrame import MembersFrame
 
 
-def generate_csv():
-    list_fin = []
-    fields = []
-    with open(Constants.MEETING_DATES_PATH) as file:
-        fields = file.readlines()
-    with open(Constants.JSON_PATH) as f:
-        temp: {} = json.load(f)
-        for key, value in temp.items():
-            t_arr = [key, value["name"]]
-
-            for d in fields:
-                if d in value["attendance"].keys():
-                    t_arr.append(value["attendance"][d])
-                else:
-                    t_arr.append("0")
-
-            list_fin.append(t_arr)
-    fields.insert(0, "Name")
-    fields.insert(0, "ID")
-
-    current_dir_path = os.path.join(
-        Constants.DOWNLOADS_PATH,
-        f'Attendance Records - {datetime.date.today().year}')
-
-    if not os.path.exists(current_dir_path):
-        os.makedirs(current_dir_path)
-
-    file_address_current = os.path.join(current_dir_path, f'{datetime.date.today()}.csv')
-
-    with open(file_address_current, "w") as f:
-        csvwriter = csv.writer(f)
-
-        csvwriter.writerow(fields)
-        csvwriter.writerows(list_fin)
-
-
 class DataGenerationFrame(customtkinter.CTkFrame):
     def __init__(self, master: any, **kwargs):
         super().__init__(master, **kwargs)
@@ -61,6 +25,13 @@ class DataGenerationFrame(customtkinter.CTkFrame):
                                                    hover_color=Constants.BLUE_HOVER_COLOR,
                                                    command=self.generate_json)
         self.getFromFile.grid(row=1, column=0, padx=10, pady=10, sticky="se")
+        self.clear_json = customtkinter.CTkButton(master=self,
+                                                  text="Clear JSON",
+                                                  fg_color=Constants.RED_COLOR,
+                                                  bg_color="transparent",
+                                                  hover_color=Constants.RED_HOVER_COLOR,
+                                                  command=clear_json_pwd)
+        self.clear_json.grid(row=1, column=0, padx=10, pady=10, sticky="sw")
         self.export_to_csv = customtkinter.CTkButton(master=self,
                                                      text="Export JSON",
                                                      fg_color=Constants.BLUE_COLOR,
@@ -90,3 +61,61 @@ class DataGenerationFrame(customtkinter.CTkFrame):
 
         with open(Constants.JSON_PATH, "w") as f:
             json.dump(jsonf, f, indent=4)
+
+
+def generate_csv():
+    list_fin = []
+    fields = []
+    with open(Constants.MEETING_DATES_PATH) as file:
+        fields = file.readlines()
+    with open(Constants.JSON_PATH) as f:
+        temp: {} = json.load(f)
+        for key, value in temp.items():
+            t_arr = [key, value["name"]]
+
+            for d in fields:
+                if d in value["attendance"].keys():
+                    t_arr.append(value["attendance"][d])
+                else:
+                    t_arr.append("0")
+
+            list_fin.append(t_arr)
+    fields.insert(0, "Name")
+    fields.insert(0, "ID")
+
+    yr_str = ""
+
+    if datetime.date.today().month <= 6:
+        yr_str = f'{datetime.date.today().year - 1}-{datetime.date.today().year}'
+    else:
+        yr_str = f'{datetime.date.today().year}-{datetime.date.today().year + 1}'
+
+    current_dir_path = os.path.join(
+        Constants.DOWNLOADS_PATH,
+        f'Attendance Records - {yr_str}')
+
+    if not os.path.exists(current_dir_path):
+        os.makedirs(current_dir_path)
+
+    file_address_current = os.path.join(current_dir_path, f'{datetime.date.today()}.csv')
+
+    with open(file_address_current, "w") as f:
+        csvwriter = csv.writer(f)
+
+        csvwriter.writerow(fields)
+        csvwriter.writerows(list_fin)
+
+
+def clear_json_pwd():
+    dialog = customtkinter.CTkInputDialog(text="Are you sure you want to do this? Enter password below:",
+                                          title="Confirmation")
+
+    entered_password = dialog.get_input()
+
+    if entered_password == "Vector8177Authorization":
+        clear_json()
+
+
+def clear_json():
+    with open(Constants.JSON_PATH, "w") as f:
+        json.dump({},f,indent=4)
